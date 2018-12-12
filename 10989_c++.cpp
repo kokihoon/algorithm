e7e4e8013f1b017d46d954cb71bdb06fc505d55a
kokihoon <rhrlgns777@gmail.com>
2018-12-12 14:32:03 +0900

    Refactoring

--- 10989_c++.cpp
+++ 10989_c++.cpp
@@ -3,20 +3,23 @@
 
 using namespace std;
 
+int a[10001];
+
 int main() {
-    int a[10001];
+
     int num;
     cin >> num;
     int temp = 0;
     for(int i = 0; i < num; i++) {
-        cin >> temp;
+        scanf("%d", &temp);
         a[temp] += 1;
     }
 
     for(int i = 1; i <= 10000; i++) {
         if(a[i] > 0){
             for(int j=0; j < a[i]; j++){
-                cout << i << '\n';
+                // cout << i << '\n';
+                printf("%d\n", i);
             }
         }
     }
