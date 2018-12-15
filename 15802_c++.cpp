6cbdf40d17bfe594132927d03a7254761d5770c5
kokihoon <rhrlgns777@gmail.com>
2018-12-16 01:06:14 +0900

    tanos

---
+++ 15802_c++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string str;
+
+    cin >> str;
+
+    if(str == "kokihoon") {
+        cout << "0" << endl;
+    }else {
+        cout << "1" << endl;
+    }
+
+    return 0;
+}
